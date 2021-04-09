24211749100b9a8da7ce90d790560ac0232af232
tausani-ah-chong <tausani93@gmail.com>
2021-04-09 22:54:08 +1200

    setup migration with aws

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,8 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+
+function handler (req: NextApiRequest, res: NextApiResponse): void {
   res.status(200).json({ name: 'John Doe' })
 }
 
